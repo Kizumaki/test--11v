b0e7da974d5f8631d7ea65c63f0935a6abf2ba7f
Chinguun Toivgoo <chinguuntoivgoo@Chinguuns-MacBook-Pro.local>
2025-02-06 13:43:58 +0800

    new comment

--- app/page.js
+++ app/page.js
@@ -16,23 +16,26 @@ const UseStateHook = () => {
   const handleToggleFriendName = () => {
     setShowFriendName(prevShowFriendName => !prevShowFriendName); 
   };
+
   return (
     <div className="flex flex-col justify-center items-center h-screen w-full bg-gray-100">
       <button 
         onClick={handleToggleName} 
-        className="bg-green-500 text-white font-bold py-2 px-4 rounded shadow-xl transition duration-300 ease-in-out transform hover:bg-green-600 hover:scale-105 mb-4"
+        className="bg-indigo-500 text-white font-bold py-2 px-4 rounded shadow-xl transition duration-300 ease-in-out transform hover:bg-green-600 hover:scale-105 mb-4"
       >
-       Show Name
+        Sigma sigma
       </button>
-    <p className="mt-4 text-xl text-black">{name}</p>
+
+      {showName && <p className="mt-4 text-xl text-black">{name}</p>}
 
       <button 
         onClick={handleToggleFriendName} 
-        className="bg-blue-500 text-white font-bold py-2 px-4 rounded shadow-xl transition duration-300 ease-in-out transform hover:bg-blue-600 hover:scale-105 mt-4"
+        className="bg-green-500 text-white font-bold py-2 px-4 rounded shadow-xl transition duration-300 ease-in-out transform hover:bg-blue-600 hover:scale-105 mt-4"
       >
-        Show Friend Name
+        Toggle Friend Name
       </button>
-      <p className="mt-4 text-xl text-black">{friendName}</p>
+
+      {showFriendName && <p className="mt-4 text-xl text-black">{friendName}</p>}
     </div>
   );
 };
