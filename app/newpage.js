4f83d84fb8816acebcd42b154db604d08158f17d
Chinguun Toivgoo <chinguuntoivgoo@Chinguuns-MacBook-Pro.local>
2025-02-07 11:09:20 +0800

    comment uldeev

--- app/newpage.js
+++ app/newpage.js
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 const UseStateHook = () => {
     const [color, setColor] = useState(" ");
     return (
-        <div className={`flex  space-x-4 w-full h-screen bg-${color}-800`}>
+        <div className={`flex space-x-4 w-full h-screen bg-${color}-800`}>
             <p>Change color:</p>
             <button className="border bg-red-800 py-4 px-6" onClick={() => {
                 setColor("red")
@@ -21,22 +21,6 @@ const UseStateHook = () => {
             }}>
              Green
             </button>
-            <button className="border bg-white py-4 px-6" onClick={() => {
-                setColor(" ")
-            }}>
-             return
-            </button>
-            {color == "red"
-              ? "123"
-              : color == "blue"
-              ? "456"
-              : color == "green" 
-              ? "789"
-              : color == ""
-              ? "1000"
-              : "000"
-            }
-            
         </div>
     
   );
