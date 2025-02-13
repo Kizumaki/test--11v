c96d9929f00ae4a58d74154fe0d5f1a04902d0ba
Chigma <153976651+Kizumaki@users.noreply.github.com>
2025-02-13 13:52:02 +0800

    Update newpage.js

--- app/newpage.js
+++ app/newpage.js
@@ -1,28 +1,42 @@
 "use client";  
 
 import React, { useState } from "react";
+
 const UseStateHook = () => {
-    const [color, setColor] = useState(" ");
-    return (
-        <div className={`flex space-x-4 w-full h-screen bg-${color}-800`}>
-            <p>Change color:</p>
-            <button className="border bg-red-800 py-4 px-6" onClick={() => {
-                setColor("red")
-            }}>
-             Red
-            </button>
-            <button className="border bg-blue-800 py-4 px-6" onClick={() => {
-                setColor("blue")
-            }}>
-             Blue
-            </button>
-            <button className="border bg-green-800 py-4 px-6" onClick={() => {
-                setColor("green")
-            }}>
-             Green
-            </button>
-        </div>
-    
+  const [count, setCount] = useState(""); 
+  const [name, setName] = useState('Taylor'); 
+  const [friendName, setFriendName] = useState('Chinguun'); 
+  const [showName, setShowName] = useState(false); 
+  const [showFriendName, setShowFriendName] = useState(false); 
+
+  const handleToggleName = () => {
+    setShowName(prevShowName => !prevShowName); 
+  };
+
+  const handleToggleFriendName = () => {
+    setShowFriendName(prevShowFriendName => !prevShowFriendName); 
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen w-full bg-gray-100">
+      <button 
+        onClick={handleToggleName} 
+        className="bg-indigo-500 text-white font-bold py-2 px-4 rounded shadow-xl transition duration-300 ease-in-out transform hover:bg-green-600 hover:scale-105 mb-4"
+      >
+        Sigma sigma
+      </button>
+
+      {showName && <p className="mt-4 text-xl text-black">{name}</p>}
+
+      <button 
+        onClick={handleToggleFriendName} 
+        className="bg-green-500 text-white font-bold py-2 px-4 rounded shadow-xl transition duration-300 ease-in-out transform hover:bg-blue-600 hover:scale-105 mt-4"
+      >
+        Toggle Friend Name
+      </button>
+
+      {showFriendName && <p className="mt-4 text-xl text-black">{friendName}</p>}
+    </div>
   );
 };
 
